afa21d6e2348df04214d5b643ef9450bd7177768
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-02-13 13:57:53 +0100

    [fix] relative url

--- quizzoto/lib/fetchQuizz.js
+++ quizzoto/lib/fetchQuizz.js
@@ -1,9 +1,9 @@
 export default async function fetchQuizz(quizzId) {
-    try {
-        const response = await fetch(`http://localhost:3000/api/quizz/${quizzId}`);
-        const jsonData = await response.json();
-        return jsonData
-    } catch (error) {
-        console.error('Erreur lors de la récupération des données:', error);
-    }
+	try {
+		const response = await fetch(`/api/quizz/${quizzId}`);
+		const jsonData = await response.json();
+		return jsonData;
+	} catch (error) {
+		console.error('Erreur lors de la récupération des données:', error);
+	}
 }
