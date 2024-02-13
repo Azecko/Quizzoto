7a08a14534168e6dc0c2130738eb69e27c10dda7
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-02-13 14:45:35 +0100

    [add] a unique key for each element

--- quizzoto/src/components/question.js
+++ quizzoto/src/components/question.js
@@ -5,30 +5,18 @@ export default function Question({ question, register }) {
         case 'radios':
             return (
                 <FormControl>
-                    <RadioGroup
-                        aria-labelledby="radio-buttons-group-label"
-                        name="radio-buttons-group"
-                    >
-                        {
-                            question.answers.map(e => {
-                                return (
-                                    <FormControlLabel {...register(question.questionTitle)} value={e} control={<Radio />} label={e} />
-                                )
-                            })
-                        }
+						{question.answers.map((e, index) => {
+							return <FormControlLabel key={index} {...register(question.questionTitle)} value={e} control={<Radio />} label={e} />;
+						})}
                     </RadioGroup>
                 </FormControl>
             )
         case 'checkboxes':
             return (
                 <FormGroup>
-                    {
-                        question.answers.map(e => {
-                            return (
-                                <FormControlLabel {...register(question.questionTitle)} value={e} control={<Checkbox />} label={e}/>
-                            )
-                        })
-                    }
+					{question.answers.map((e, index) => {
+						return <FormControlLabel key={index} {...register(question.questionTitle)} value={e} control={<Checkbox />} label={e} />;
+					})}
                 </FormGroup>
             )
         case 'textfield':
