aab88e5be380f9329e41447c6aa54c3899517dd1
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-02-14 10:07:08 +0100

    [fix] register

--- quizzoto/src/components/question.js
+++ quizzoto/src/components/question.js
@@ -1,27 +1,27 @@
-import { FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, FormGroup, Checkbox, TextField } from "@mui/material"
+import { FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, FormGroup, Checkbox, TextField, Input } from '@mui/material';
 
-export default function Question({ question, register }) {
-    switch(question.questionType) {
-        case 'radios':
-            return (
-                <FormControl>
+export default function Question({ question, register, questionId }) {
+	questionId = questionId.toString();
+	switch (question.questionType) {
+		case 'radios':
+			return (
+				<FormControl>
+					<RadioGroup aria-labelledby="radio-buttons-group-label" name="radio-buttons-group">
 						{question.answers.map((e, index) => {
-							return <FormControlLabel key={index} {...register(question.questionTitle)} value={e} control={<Radio />} label={e} />;
+							return <FormControlLabel {...register(questionId)} key={index} value={e} control={<Radio />} label={e} />;
 						})}
-                    </RadioGroup>
-                </FormControl>
-            )
-        case 'checkboxes':
-            return (
-                <FormGroup>
+					</RadioGroup>
+				</FormControl>
+			);
+		case 'checkboxes':
+			return (
+				<FormGroup>
 					{question.answers.map((e, index) => {
-						return <FormControlLabel key={index} {...register(question.questionTitle)} value={e} control={<Checkbox />} label={e} />;
+						return <FormControlLabel {...register(questionId)} key={index} value={e} control={<Checkbox />} label={e} />;
 					})}
-                </FormGroup>
-            )
-        case 'textfield':
-            return (
-                <TextField {...register(question.questionTitle)} label="Réponse ici" variant="outlined" />
-            )
-    }
+				</FormGroup>
+			);
+		case 'textfield':
+			return <TextField {...register(questionId)} label="Réponse ici" variant="outlined" />;
+	}
 }
