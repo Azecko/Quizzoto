150dddf8e66f804a7dda4248d3e8d5e04e283196
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-02-16 12:42:28 +0100

    [add] a table of results

--- quizzoto/src/components/result.js
+++ quizzoto/src/components/result.js
@@ -1,45 +1,51 @@
+import TableCell from '@mui/material/TableCell';
+
 export default function Result({ result }) {
-    if(result.userAnswer == null || result.userAnswer == false || result.userAnswer.length == 0) {
-        result.userAnswer = 'Aucune';
-    }
+	if (result.userAnswer == null || result.userAnswer == false || result.userAnswer.length == 0) {
+		result.userAnswer = 'Aucune';
+	}
 
-    function ifWrong(result) {
-        switch (typeof(result.correctAnswer)) {
-            case 'string':
-                return (
-                    <div>
-                        <h3>Réponse correcte :</h3> {result.correctAnswer}
-                    </div>
-                )
-            case 'object':
-                return (
-                    <div>
-                        <h3>Réponse correcte :</h3>
-                        <ul>
-                            {result.correctAnswer.map(e => { return <li>{e}</li> })}
-                        </ul>
-                    </div>
-                )
-        }
-    }
+	function ifWrong(result) {
+		switch (typeof result.correctAnswer) {
+			case 'string':
+				return (
+					<>
+						<TableCell align="right">{result.correctAnswer}</TableCell>
+						<TableCell align="right"></TableCell>
+					</>
+				);
+			case 'object':
+				return (
+					<>
+						<TableCell align="right">
+							{result.correctAnswer.map((e, index) => {
+								return <span key={index}>{e}, </span>;
+							})}
+						</TableCell>
+						<TableCell align="right"></TableCell>
+					</>
+				);
+		}
+	}
 
-    switch(typeof(result.userAnswer)) {
-        case 'string':
-            return (
-                <div>
-                    <h4>Votre réponse :</h4> {result.userAnswer}
-                    {!result.answeredCorrectly && ifWrong(result)}
-                </div>
-            )
-        case 'object':
-            return (
-                <div>
-                    <h4>Votre réponse :</h4>
-                    <ul>
-                        {result.userAnswer.map(e => { return <li>{e}</li> })}
-                    </ul>
-                    {!result.answeredCorrectly && ifWrong(result)}
-                </div>
-            )
-    }
+	switch (typeof result.userAnswer) {
+		case 'string':
+			return (
+				<>
+					<TableCell align="right">{result.userAnswer}</TableCell>
+					{!result.answeredCorrectly && ifWrong(result)}
+				</>
+			);
+		case 'object':
+			return (
+				<>
+					<TableCell align="right">
+						{result.userAnswer.map((e, index) => {
+							return <span key={index}>{e}, </span>;
+						})}
+					</TableCell>
+					{!result.answeredCorrectly && ifWrong(result)}
+				</>
+			);
+	}
 }
