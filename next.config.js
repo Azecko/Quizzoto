704d306267bcbf07b0a4c6174e7f087069178c55
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-02-20 17:09:22 +0100

    [add] transpilePackages for @mui/x-charts

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	transpilePackages: ['@mui/x-charts'],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
