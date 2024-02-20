047fab8e8afbd50a966fa853037dfa08d71af804
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-02-20 17:09:51 +0100

    [add] @mui/x-charts/PieChart

--- src/pages/result/[id].js
+++ src/pages/result/[id].js
@@ -13,6 +13,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { PieChart } from '@mui/x-charts/PieChart';
 
 export default function Quizz() {
 	const [result, setResult] = useState();
