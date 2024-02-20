20508f547a32c94e1b1d27a02ec4230e4bfe41f8
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-02-20 11:22:12 +0100

    [update] search bar still displayed

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,25 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 import User from './user';
 import SearchBar from './search';
 import Logo from './logo';
 import Box from '@mui/material/Box';
 
-export default function Header({ quizzTitle }) {
+import { useState } from 'react';
+
+export default function Header() {
 	const [searchContent, setSearchContent] = useState('');
 
 	const handleSearchChange = (content) => {
 		setSearchContent(content);
 	};
 
-	console.log(quizzTitle);
-
 	return (
-		<header style={{ width: '100%', display: 'flex', height: '110px' }}>
+		<header style={{ width: '100%', display: 'flex', height: '110px', marginBottom: '20px' }}>
 			<Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={2} style={{ width: '100%' }}>
 				<Box gridColumn="span 2">
 					<Logo />
 				</Box>
-				<Box gridColumn="span 8">{quizzTitle != '' ? quizzTitle : quizzTitle == '' ? quizzTitle : <SearchBar onSearchChange={handleSearchChange} />}</Box>
+				<Box gridColumn="span 8">
+					<SearchBar onSearchChange={handleSearchChange} />
+				</Box>
+
 				<Box gridColumn="span 2">
 					<User user={{ username: 'Dwesh45' }} />
 				</Box>
