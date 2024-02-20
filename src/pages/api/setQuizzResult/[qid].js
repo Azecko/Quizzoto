053849d8da60c12ebfd68a5ab2538d09efcd0cd0
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-02-20 15:16:53 +0100

    [add] check quizzSlug in setresults

--- src/pages/api/setQuizzResult/[qid].js
+++ src/pages/api/setQuizzResult/[qid].js
@@ -2,19 +2,34 @@ import db from '../../../../lib/mongodb';
 
 var mongodb = require('mongodb');
 
+async function getQuizzIdFromSlug(quizzSlug) {
+	let id = await db.collection('quizzs').findOne(
+		{ quizzSlug: quizzSlug },
+		{
+			projection: {
+				_id: 1,
+			},
+		}
+	);
+	return id._id.toString();
+}
+
 export default async function handler(req, res) {
 	if (req.method !== 'POST') {
 		return res.status(405).json({ statusCode: 405, message: `This request method is not valid on this route.` });
 	}
 
-	if (!mongodb.ObjectId.isValid(req.query.qid)) {
+	let quizzId = null;
+
+	if (mongodb.ObjectId.isValid(req.query.qid)) {
+		quizzId = req.query.qid;
+	} else if (getQuizzIdFromSlug(req.query.qid) != null) {
+		quizzId = await getQuizzIdFromSlug(req.query.qid);
+	} else {
 		return res.status(422).json({ statusCode: 422, message: `Please provide a valid quizz's object id.` });
 	}
 
-	const quizz = await db.collection('quizzs').findOne({ _id: new mongodb.ObjectId(req.query.qid) });
-	if (quizz == null) {
-		return res.status(422).json({ statusCode: 422, message: `Please provide a valid quizz's object id.` });
-	}
+	const quizz = await db.collection('quizzs').findOne({ _id: new mongodb.ObjectId(`${quizzId}`) });
 
 	function isJsonString(str) {
 		try {
