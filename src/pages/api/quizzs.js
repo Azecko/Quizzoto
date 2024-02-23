1245299bff32133df0ff8d76e104dae73c417c21
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-02-23 11:06:17 +0100

    [add] Api to get all the quizzes

--- src/pages/api/quizzs.js
+++ src/pages/api/quizzs.js
@@ -1,6 +1,20 @@
-import db from "../../../lib/mongodb";
+import db from '../../../lib/mongodb';
 
 export default async function handler(req, res) {
-  const quizzs = await db.collection('quizzs').find({}).toArray();
-  res.status(200).json(quizzs)
+	const quizzs = await db
+		.collection('quizzs')
+		.find(
+			{},
+			{
+				projection: {
+					quizzTitle: 1,
+					quizzDescription: 1,
+					quizzImg: 1,
+					quizzInfo: 1,
+				},
+			}
+		)
+		.toArray();
+
+	res.status(200).json(quizzs);
 }
