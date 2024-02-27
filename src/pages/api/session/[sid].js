150932e4d57835c3faba81c3eb7fcf3d7186a169
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-02-27 08:50:45 +0100

    [fix] get all session and quiz information

--- src/pages/api/session/[sid].js
+++ src/pages/api/session/[sid].js
@@ -5,5 +5,23 @@ var mongodb = require('mongodb');
 export default async function Session(req, res) {
 	const results = await db.collection('results').find({ sessionId: req.query.sid }).toArray();
 
-	return res.status(200).send(results);
+	const quizzId = await db.collection('session').findOne({ _id: new mongodb.ObjectId(req.query.sid) });
+
+	const quizz = await db.collection('quizzs').findOne(
+		{ _id: new mongodb.ObjectId(quizzId.quizzId) },
+		{
+			projection: {
+				quizzTitle: 1,
+				quizzDescription: 1,
+				quizzImg: 1,
+				quizzInfo: 1,
+				quizzSlug: 1,
+			},
+		}
+	);
+
+	quizz.results = results;
+	quizz.sessionId = req.query.sid;
+
+	return res.status(200).send(quizz);
 }
