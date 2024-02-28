b73436f307255d7094628f297489b302d503d96c
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-02-28 15:02:04 +0100

    [add] a user list to display all users

---
+++ src/components/userList.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { useRouter } from 'next/router';
+
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+
+export default function UserList({ users }) {
+	const router = useRouter();
+
+	if (!users) {
+		return;
+	}
+
+	const handleClick = (username) => {
+		router.push(`/user/${username}`);
+	};
+
+	return (
+		<List dense sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+			{users.map((e, index) => {
+				return (
+					<ListItem key={index} disablePadding>
+						<ListItemButton onClick={() => handleClick(e.username)}>
+							<ListItemAvatar>
+								<Avatar alt={e.username} src={e.image} />
+							</ListItemAvatar>
+							<ListItemText primary={e.username} />
+						</ListItemButton>
+					</ListItem>
+				);
+			})}
+		</List>
+	);
+}
