cde599c4aff98174add778403ecbe10085c2f6df
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-02-28 15:02:38 +0100

    [fix] user style

--- src/components/header/user.js
+++ src/components/header/user.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Avatar from 'boring-avatars';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 const userContainerStyles = {
 	display: 'flex',
@@ -12,16 +13,27 @@ const userContainerStyles = {
 const usernameStyles = {
 	marginLeft: '8px',
 	color: '#8d9299',
-	fontSize: '1.3rem',
+	fontSize: '1rem',
+};
+
+const imgStyle = {
+	width: '4rem',
+	borderRadius: '50%',
 };
 
 export default function User({ user }) {
-	const username = user.username;
+	const { data: session } = useSession();
 
-	return (
-		<Box style={userContainerStyles}>
-			<Avatar size={70} name={username} variant="beam" colors={['#264653', '#f4a261', '#e76f51']} />
-			<p style={usernameStyles}>{username}</p>
-		</Box>
-	);
+	if (session) {
+		const username = session.user.name;
+		return (
+			<Box style={userContainerStyles}>
+				<img src={session.user.image} style={imgStyle} />
+				{/* <Avatar size={70} name={username} variant="beam" colors={['#264653', '#f4a261', '#e76f51']} /> */}
+				<p style={usernameStyles}>{session.user.name}</p>
+			</Box>
+		);
+	} else {
+		return <></>;
+	}
 }
