edee139e63e611c4165a05a8af40d45ad98dfb61
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-03-01 14:34:51 +0100

    [add] id and username in session

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import { v4 as uuidv4 } from 'uuid';
 import db from '../../../../lib/mongodb';
 
 export const authOptions = {
@@ -10,33 +11,46 @@ export const authOptions = {
 		}),
 	],
 	callbacks: {
-		async signIn(user, account, profile) {
-			const existingUser = await db.collection('users').findOne({ email: user.user.email });
+		async signIn({ user, account, profile }) {
+			console.log(user, account, profile);
+			const existingUser = await db.collection('users').findOne({ email: user.email });
+
 			if (existingUser) {
 				const updatedUser = {
 					$set: {
-						username: user.profile.login,
-						image: user.user.image,
-						provider: user.account.provider,
-						company: user.profile.company,
-						name: user.profile.name,
+						username: profile.login,
+						image: user.image,
+						provider: account.provider,
+						company: profile.company,
+						name: profile.name,
+						id: uuidv4(),
 					},
 				};
-				await db.collection('users').updateOne({ email: user.user.email }, updatedUser);
+				await db.collection('users').updateOne({ email: user.email }, updatedUser);
 			} else {
 				const newUser = {
-					email: user.user.email,
-					username: user.profile.login,
-					image: user.user.image,
-					provider: user.account.provider,
-					company: user.profile.company,
-					name: user.profile.name,
+					id: uuidv4(),
+					email: user.email,
+					username: profile.login,
+					image: user.image,
+					provider: account.provider,
+					company: profile.company,
+					name: profile.name,
 					points: 0,
 					displayPoints: false,
 				};
 				await db.collection('users').insertOne(newUser);
 			}
 			return Promise.resolve(true);
+		},
+
+		async session({ session, token, user }) {
+			let newSession = session;
+			const existingUser = await db.collection('users').findOne({ email: session.user.email });
+
+			newSession.user.id = existingUser.id;
+			newSession.user.username = existingUser.username;
+			return session;
 		},
 	},
 };
