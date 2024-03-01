e4f1dc4c49f952b8671f09631952682228225e5e
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-03-01 09:55:58 +0100

    [Add] a menu to see the logout button

--- src/components/loginButton.js
+++ src/components/loginButton.js
@@ -1,4 +1,9 @@
 import { useSession, signIn, signOut } from 'next-auth/react';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import User from './header/user';
 
 export default function LoginButton() {
 	const BtnStyle = {
@@ -26,12 +31,41 @@ export default function LoginButton() {
 	};
 
 	const { data: session } = useSession();
+
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const open = Boolean(anchorEl);
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+	
+
 	if (session) {
 		return (
-			<>
-				{/* Signed in as {session.user.email} <br /> */}
-				{/* <button onClick={() => signOut()}>Sign out</button> */}
-			</>
+			<div>
+				<Button id="demo-positioned-button" aria-controls={open ? 'demo-positioned-menu' : undefined} aria-haspopup="true" aria-expanded={open ? 'true' : undefined} onClick={handleClick}>
+					<User />
+				</Button>
+				<Menu
+					id="demo-positioned-menu"
+					aria-labelledby="demo-positioned-button"
+					anchorEl={anchorEl}
+					open={open}
+					onClose={handleClose}
+					anchorOrigin={{
+						vertical: 'top',
+						horizontal: 'left',
+					}}
+					transformOrigin={{
+						vertical: 'top',
+						horizontal: 'left',
+					}}>
+					<MenuItem onClick={() => signOut()}>Logout</MenuItem>
+				</Menu>
+			</div>
 		);
 	}
 	return (
