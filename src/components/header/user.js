e835eda0baaf5b077fc44ce275b418c3058eba52
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-03-01 10:25:01 +0100

    [fix] remove boring avatar and add style

--- src/components/header/user.js
+++ src/components/header/user.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import Box from '@mui/material/Box';
-import Avatar from 'boring-avatars';
-import { useSession, signIn, signOut } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 
 const userContainerStyles = {
 	display: 'flex',
 	alignItems: 'center',
+	justifyContent: 'center',
 	height: '100%',
-	marginLeft: '20%',
 };
 
 const usernameStyles = {
 	marginLeft: '8px',
 	color: '#8d9299',
 	fontSize: '1rem',
+	overflow: 'hidden',
+	textOverflow: 'ellipsis',
 };
 
 const imgStyle = {
 	width: '4rem',
+	height: '4rem',
 	borderRadius: '50%',
 };
 
@@ -29,7 +31,6 @@ export default function User({ user }) {
 		return (
 			<Box style={userContainerStyles}>
 				<img src={session.user.image} style={imgStyle} />
-				{/* <Avatar size={70} name={username} variant="beam" colors={['#264653', '#f4a261', '#e76f51']} /> */}
 				<p style={usernameStyles}>{session.user.name}</p>
 			</Box>
 		);
